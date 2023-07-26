3fcdfa7227f76a7d113922ada95164b1a0435ac9
YYYXB <1746155023@qq.com>
2023-07-27 02:57:29 +0800

    update bili - DQN

--- bili/06_DQN/02_双模型_平衡车.py
+++ bili/06_DQN/02_双模型_平衡车.py
@@ -1,15 +1,12 @@
 import gym
 import torch
 import random
-from IPython import display
-from matplotlib import pyplot as plt
 import numpy as np
 
 
 # 定义环境
 class MyWrapper(gym.Wrapper):
     def __init__(self):
-        # env = gym.make('CartPole-v1', render_mode='rgb_array')
         env = gym.make('CartPole-v1', render_mode='human')
         super().__init__(env)
         self.env = env
@@ -29,33 +26,27 @@ class MyWrapper(gym.Wrapper):
         return state, reward, done, info
 
 
-# 打印游戏
-def show(env):
-    plt.imshow(env.render())
-    plt.show()
-
-
 # 测试游戏环境
-def test_env(env):
+def env_info(env):
     state = env.reset()
     print('这个游戏的状态用4个数字表示,我也不知道这4个数字分别是什么意思,反正这4个数字就能描述游戏全部的状态')
-    print('state=', state)
     # state= [ 0.03490619  0.04873464  0.04908862 -0.00375859]
+    print('state =', state)
     print('这个游戏一共有2个动作,不是0就是1')
-    print('env.action_space=', env.action_space)
-    # env.action_space= Discrete(2)
+    # env.action_space = Discrete(2)
+    print('env.action_space =', env.action_space)
     print('随机一个动作')
     action = env.action_space.sample()
-    print('action=', action)
     # action= 1
+    print('action=', action)
     print('执行一个动作,得到下一个状态,奖励,是否结束')
     state, reward, over, info = env.step(action)
-    print('state=', state)
     # state= [ 0.02018229 -0.16441101  0.01547085  0.2661691 ]
-    print('reward=', reward)
+    print('state=', state)
     # reward= 1.0
-    print('over=', over)
+    print('reward=', reward)
     # over= False
+    print('over=', over)
 
 
 def create_model():
@@ -71,7 +62,6 @@ def create_model():
 def get_action(state, model):
     if random.random() < 0.01:
         return random.choice([0, 1])
-
     # 走神经网络,得到一个动作
     state = torch.FloatTensor(state).reshape(1, 4)
     return model(state).argmax().item()
@@ -84,34 +74,26 @@ datas = []
 # 向样本池中添加N条数据,删除M条最古老的数据
 def update_data(env, model):
     old_count = len(datas)
-
     # 玩到新增了N个数据为止
     while len(datas) - old_count < 200:
         # 初始化游戏
         state = env.reset()
-
         # 玩到游戏结束为止
         over = False
         while not over:
             # 根据当前状态得到一个动作
             action = get_action(state, model)
-
             # 执行动作,得到反馈
             next_state, reward, over, _ = env.step(action)
-
             # 记录数据样本
             datas.append((state, action, reward, next_state, over))
-
             # 更新游戏状态,开始下一个动作
             state = next_state
-
     update_count = len(datas) - old_count
     drop_count = max(len(datas) - 10000, 0)
-
     # 数据上限,超出时从最古老的开始删除
     while len(datas) > 10000:
         datas.pop(0)
-
     return update_count, drop_count
 
 
@@ -119,7 +101,6 @@ def update_data(env, model):
 def get_sample():
     # 从样本池中采样
     samples = random.sample(datas, 64)
-
     # [b, 4]
     # state = torch.FloatTensor([i[0] for i in samples]).reshape(-1, 4)
     samples_np = np.array([i[0] for i in samples])
@@ -132,7 +113,6 @@ def get_sample():
     next_state = torch.FloatTensor(np.array([i[3] for i in samples])).reshape(-1, 4)
     # [b, 1]
     over = torch.LongTensor([i[4] for i in samples]).reshape(-1, 1)
-
     return state, action, reward, next_state, over
 
 
@@ -140,14 +120,12 @@ def get_value(state, action, model):
     # 使用状态计算出动作的 logic
     # [b, 4] -> [b, 2]
     value = model(state)
-
     # 根据实际使用的action取出每一个值
     # 这个值就是模型评估的在该状态下,执行动作的分数
     # 在执行动作前,显然并不知道会得到的反馈和next_state
     # 所以这里不能也不需要考虑next_state和reward
     # [b, 2] -> [b, 1]
     value = value.gather(dim=1, index=action)
-
     return value
 
 
@@ -161,49 +139,35 @@ def get_target(reward, next_state, over, next_model):
     # [b, 4] -> [b, 2]
     with torch.no_grad():
         target = next_model(next_state)
-
     # 取所有动作中分数最大的
     # [b, 2] -> [b, 1]
     target = target.max(dim=1)[0]
     target = target.reshape(-1, 1)
-
     # 下一个状态的分数乘以一个系数,相当于权重
     target *= 0.98
-
     # 如果next_state已经游戏结束,则next_state的分数是0
     # 因为如果下一步已经游戏结束,显然不需要再继续玩下去,也就不需要考虑next_state了.
     # [b, 1] * [b, 1] -> [b, 1]
     target *= (1 - over)
-
     # 加上reward就是最终的分数
     # [b, 1] + [b, 1] -> [b, 1]
     target += reward
-
     return target
 
 
-def test(play, env, model):
+def test(env, model):
     # 初始化游戏
     state = env.reset()
-
     # 记录反馈值的和,这个值越大越好
     reward_sum = 0
-
     # 玩到游戏结束为止
     over = False
     while not over:
         # 根据当前状态得到一个动作
         action = get_action(state, model)
-
         # 执行动作,得到反馈
         state, reward, over, _ = env.step(action)
         reward_sum += reward
-
-        # 打印动画
-        if play and random.random() < 0.2:  # 跳帧
-            display.clear_output(wait=True)
-            show(env)
-
     return reward_sum
 
 
@@ -211,33 +175,27 @@ def train(env, model, next_model):
     model.train()
     optimizer = torch.optim.Adam(model.parameters(), lr=2e-3)
     loss_fn = torch.nn.MSELoss()
-
     # 训练N次
     for epoch in range(500):
         # 更新N条数据
         update_count, drop_count = update_data(env, model)
-
         # 每次更新过数据后,学习N次
         for i in range(200):
             # 采样一批数据
             state, action, reward, next_state, over = get_sample()
-
             # 计算一批样本的value和target
             value = get_value(state, action, model)
             target = get_target(reward, next_state, over, next_model)
-
             # 更新参数
             loss = loss_fn(value, target)
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
-
             # 把model的参数复制给next_model
             if (i + 1) % 10 == 0:
                 next_model.load_state_dict(model.state_dict())
-
         if epoch % 50 == 0:
-            test_result = sum([test(False, env, next_model) for _ in range(20)]) / 20
+            test_result = sum([test(env, next_model) for _ in range(20)]) / 20
             print(f"epoch = {epoch}, len(datas) = {len(datas)}, "
                   f"update_count = {update_count}, drop_count = {drop_count}, "
                   f"test_result = {test_result}")
@@ -246,7 +204,7 @@ def train(env, model, next_model):
 def main():
     env = MyWrapper()
     env.reset()
-    test_env(env)
+    env_info(env)
     # 计算动作的模型,也是真正要用的模型
     model = create_model()
     # 经验网络,用于评估一个状态的分数
@@ -264,7 +222,7 @@ def main():
     # print(f"{0}， {1}， {2}， {3}", state, action, reward, next_state, over)
     print("get_value() = ", get_value(state, action, model).shape)
     print("get_target() = ", get_target(reward, next_state, over, next_model).shape)
-    print("test() = ", test(False, env, model))
+    print("test() = ", test(env, model))
     train(env, model, next_model)
 
 
