9e0e980649fcada1e6a448553d21908273406b53
YYYXB <1746155023@qq.com>
2023-12-17 18:24:33 +0800

    REINFORCE, utility on-policy

--- YYYRL/REINFORCE.py
+++ YYYRL/REINFORCE.py
@@ -3,7 +3,6 @@ import torch
 import torch.nn.functional as F
 import numpy as np
 import matplotlib.pyplot as plt
-from tqdm import tqdm
 import utility
 
 
@@ -50,70 +49,33 @@ class REINFORCE:
         self.optimizer.step()  # 梯度下降
 
 
-def main():
+def train(env_name: str):
     learning_rate = 1e-3
     num_episodes = 1000
     hidden_dim = 128
     gamma = 0.98
     device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
 
-    env_name = "CartPole-v0"
     env = gym.make(env_name)
     env.seed(0)
     torch.manual_seed(0)
     state_dim = env.observation_space.shape[0]
     action_dim = env.action_space.n
+    print(f"device: {device}\n"
+          f"env_name = {env_name}, state_dim = {state_dim}, action_dim = {action_dim}")
     agent = REINFORCE(state_dim, hidden_dim, action_dim, learning_rate, gamma, device)
 
-    return_list = []
-    for i in range(10):
-        with tqdm(total=int(num_episodes / 10), desc='Iteration %d' % i) as pbar:
-            for i_episode in range(int(num_episodes / 10)):
-                episode_return = 0
-                transition_dict = {
-                    'states': [],
-                    'actions': [],
-                    'next_states': [],
-                    'rewards': [],
-                    'dones': []
-                }
-                state = env.reset()
-                done = False
-                while not done:
-                    action = agent.take_action(state)
-                    next_state, reward, done, _ = env.step(action)
-                    transition_dict['states'].append(state)
-                    transition_dict['actions'].append(action)
-                    transition_dict['rewards'].append(reward)
-                    transition_dict['next_states'].append(next_state)
-                    transition_dict['dones'].append(done)
-                    state = next_state
-                    episode_return += reward
-                return_list.append(episode_return)
-                agent.update(transition_dict)
-                if (i_episode + 1) % 10 == 0:
-                    pbar.set_postfix({
-                        'episode': '%d' % (num_episodes / 10 * i + i_episode + 1),
-                        'return': '%.3f' % np.mean(return_list[-10:])
-                    })
-                pbar.update(1)
+    return_list = utility.train_on_policy_agent(env, agent, num_episodes)
+    # np.mean(return_list) = 141.86
+    return return_list
 
-    print("---------------------")
-    print(f"回报的平均值 = {np.mean(return_list)}")
 
-    episodes_list = list(range(len(return_list)))
-    plt.plot(episodes_list, return_list)
-    plt.xlabel('Episodes')
-    plt.ylabel('Returns')
-    plt.title('REINFORCE on {}'.format(env_name))
-    plt.show()
-
-    mv_return = utility.moving_average(return_list, 9)
-    plt.plot(episodes_list, mv_return)
-    plt.xlabel('Episodes')
-    plt.ylabel('Returns')
-    plt.title('REINFORCE on {}'.format(env_name))
-    plt.show()
+def main():
+    start_time = utility.get_current_time()
+    env_name = "CartPole-v0"
+    return_list = train(env_name)
+    utility.time_difference(start_time)
+    utility.plot(return_list, "REINFORCE", env_name)
 
 
 if __name__ == "__main__":
